a4d9ce565a57c410fe2e05fd162a9ae579dbbdc9
r57ty7 <16367098+r57ty7@users.noreply.github.com>
2021-04-25 23:59:56 +0900

    :sparkles: ワード検索して表示する

--- src/pages/SearchBook.svelte
+++ src/pages/SearchBook.svelte
@@ -1,9 +1,47 @@
-<script>
+<script lang="ts">
   import SearchBar from '../components/SearchBar.svelte'
+  import type { BookItem } from '../repositories/book'
+  import RepositoryFactory, { BOOK } from '../repositories/RepositoryFactory'
+
+  const BookRepository = RepositoryFactory[BOOK]
 
   let q = 'Svelte'
+  let empty = true
+  let books: BookItem[]
+  let promise: Promise<void>
+
+  const handleSubmit = () => {
+    if (!q.trim()) return
+    promise = getBooks()
+  }
+
+  const getBooks = async () => {
+    books = []
+    empty = false
+    const result = await BookRepository.get({ q })
+    empty = result.totalItems === 0
+    books = result.items
+  }
 </script>
 
-<form>
+<form on:submit|preventDefault={handleSubmit}>
   <SearchBar bind:value={q} />
 </form>
+
+<div class="text-center mt-4">
+  {#if empty}
+    <div>検索結果が見つかりません</div>
+  {:else}
+    {#each books as book (book.id)}
+      <div>{book.volumeInfo.title}</div>
+    {/each}
+  {/if}
+
+  {#await promise}
+    <div>loading...</div>
+  {:catch e}
+    <span class="text-red-600 text-sm">
+      {e.message}
+    </span>
+  {/await}
+</div>
